19f3c650e50f6cf529957f445c683990f7e9c202
user <user@192.168.0.156>
2021-08-03 04:35:03 +0800

    Spring

---
+++ spring/src/main/java/com/study/spring/Test.java
@@ -0,0 +1,13 @@
+package com.study.spring;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.setId(1);
+		person.setName("Java");
+		System.out.println(person);
+
+	}
+
+}
